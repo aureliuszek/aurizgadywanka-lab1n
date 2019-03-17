aa9542dd6709d6b26aa920bf8c64ae5a27149551
aureliuszek <48626117+aureliuszek@users.noreply.github.com>
2019-03-17 12:27:13 +0100

    zrobilem to xd

---
+++ GraZaDuzoZaMalo/GraMonolitycznie/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraMonolitycznie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("witaj");
+
+            Console.WriteLine("Podaj swe imie, wedrowcze: ");
+            string imie = Console.ReadLine();
+            Console.WriteLine($"Witaj {imie}!!!");
+        }
+    }
+}
